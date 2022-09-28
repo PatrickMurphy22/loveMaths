27855738ecbf8ec854ad374b6d1a9000da003a77
PatrickMurphy22 <Murphy866530@gmail.com>
2022-09-28 10:44:29 +0000

    added event listener for buttons

--- js.js
+++ js.js
@@ -1,31 +1,46 @@
-runGame(){
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
 
-}
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("you clicked submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
 
-checkAnswer(){
+// runGame(){
 
-}
+// }
 
-calculateCorrectAnswer(){
+// checkAnswer(){
+
+// }
+
+// calculateCorrectAnswer(){
     
-}
+// }
 
-incrementScore(){
+// incrementScore(){
     
-}
+// }
 
-incrementWrongAnswer(){
+// incrementWrongAnswer(){
     
-}
+// }
 
-disaplayAdditionQuestion(){
+// disaplayAdditionQuestion(){
     
-}
+// }
 
-disaplaySubtractQuestion(){
+// disaplaySubtractQuestion(){
     
-}
+// }
 
-disaplayMultiplyQuestion(){
+// disaplayMultiplyQuestion(){
     
-}
+// }
