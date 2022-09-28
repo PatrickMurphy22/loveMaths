142bbcc5893da22583b1ce3c2ebd11e8aa833e22
PatrickMurphy22 <Murphy866530@gmail.com>
2022-09-28 13:38:53 +0000

    added more functions

--- js.js
+++ js.js
@@ -35,6 +35,16 @@ function checkAnswer(){
 
 function calculateCorrectAnswer(){
     
+    let operand1 = parseInt(document.getElementById("operand1").innerText)
+    let operand2 = parseInt(document.getElementById("operand2").innerText)
+    let operator = document.getElementById("operator").innerText
+
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert(`Unemplemented operator ${operator}`);
+        throw `Unemplemented operator ${operator}. Aborting`
+    }
 }
 
 function incrementScore(){
@@ -46,17 +56,33 @@ function incrementWrongAnswer(){
 }
 
 function disaplayAdditionQuestion(operand1, operand2){
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
 
-
 }
 
 function disaplaySubtractQuestion(){
-    
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
+
 }
 
 function disaplayMultiplyQuestion(){
-    
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+
+}
+
+function disaplayDivideQuestion(){
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "÷";
+
 }
